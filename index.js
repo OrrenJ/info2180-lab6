1d74bc060a4883c71a4cf600cada53dc7ebf12b8
Orren Joseph <ojey11@gmail.com>
2016-11-09 02:10:38 -0500

    Exercise 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+
     $("#search").click(function(){
         $.ajax({
             method: "GET",
@@ -9,4 +9,28 @@ $(document).ready(function(){
             $("#result").html(msg);
         });
     });
+
+    $("#getall").click(function(){
+        $.ajax({
+            method: "GET",
+            url: "request.php",
+            dataType: "xml",
+            data: { all: true }
+        }).done(function(xml){
+        	var def_arr = $(xml).find("definitions");
+        	var terms = $(def_arr).children();
+        	var ol = $("<ol>");
+        	$(terms).each(function(){
+        		var li = $("<li>");
+        		var name = $(this).find('name').text();
+        		var definition = $(this).find('definition').text();
+        		var author = $(this).find('author').text();
+        		$(li).append("<h3>"+name.toUpperCase()+"</h3>");
+        		$(li).append("<p>"+definition+"</h3>");
+        		$(li).append("<p>- "+author+"</p>");
+        		$(ol).append(li);
+        	});
+        	$("#result").html(ol);
+        });
+	});
 });
